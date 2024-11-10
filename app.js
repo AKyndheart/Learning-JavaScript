586ff20bda595ff2a61893cf07be205cbd9dcdbd
Andrew <klyzub102991@aol.com>
2024-11-10 18:01:10 -0500

    fix

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ async function main() {
     const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
     const data = await response.json()
     console.log(data)
-    emailRef.innerHTML = data.email
+    emailRef.innerHTML = data.name
 }
 
 main()
