e8890c88fcb2f8e7054c408532f0994215b08b55
Andrew <klyzub102991@aol.com>
2024-11-10 17:19:15 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const emailRef = document.querySelector(".email");
+console.log(emailRef)
+
+async function main() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+    const data = await response.json()
+    console.log(data)
+    emailRef.innerHTML = data.email
+}
+
+main()
